f70df84fe20bbc4be08037c17ec351a3fecf2f3c
vsanchezrod <v.sanchez.rod@gmail.com>
2019-01-24 14:04:48 +0100

    Template de login, y control de errores en los templates de registro y login, así como gestión de mensajes con flash (with messages)

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # url_for => Se puede usar desde cualquier parte de nuestra web (MIRAR LAYOUT.HTML)
-from flask import Flask, render_template, url_for
+# flash => Para mandar mensajes de alerta
+# redirect ==> Para redirigir la página
+from flask import Flask, render_template, url_for, flash, redirect
 
 # Se importa de forms las clases creadas
 from forms import RegistrationForm, LoginForm
@@ -37,17 +39,36 @@ def about():
     return render_template("about.html", title="About")
 
 
-@app.route("/register")
+# Se puede exponer los métodos que están permitidos
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     # Se crea una instancia del formulario de registro y se le pasa por el render
     form = RegistrationForm()
+
+    # Comprueba estado de la validación en la acción del submit (true o false)
+    if form.validate_on_submit():
+        # Si el estado es correcto, se manda un mensaje, con el estado de bootstrap success
+        flash(f'Account created for {form.username.data}!', 'success')
+        # Si ha tenido éxito redirigimos
+        return redirect(url_for('home'))
+
     return render_template("register.html", title="register", form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     # Se crea una instancia del formulario de login y se le pasa por el render
     form = LoginForm()
+
+    # Validación provisional de login por no tener BBDD
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            # Si el estado es correcto, se manda un mensaje, con el estado de bootstrap success
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful!. Please check username and password', 'danger')
+
     return render_template("login.html", title="login", form=form)
 
 
